1bf3770730e1e3d12c3b47fc143abf4716ffab95
Felipe Oliveira <flpbrandao@hotmail.com>
2021-06-13 13:21:13 -0300

    Fixed delegation and encapsulation - created file class

--- src/entities/ExcelFile.java
+++ src/entities/ExcelFile.java
@@ -1,11 +1,21 @@
 package entities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelFile {
-	
+
+	private String path;
+
 	public ExcelFile(String path) {
 		this.path = path;
 	}
-	private String path;
 
 	public String getPath() {
 		return path;
@@ -15,7 +25,66 @@ public class ExcelFile {
 		this.path = path;
 	}
 
+	public void ReadFromFile() {
+		try {
+			String excelFilePath = this.getPath();
+			FileInputStream inputstream = new FileInputStream(excelFilePath); // Inicializa leitor de arquivos genérico
+																				// - fs
+			XSSFWorkbook workbook = new XSSFWorkbook(inputstream);
+			; // Inicializa objeto workbook (Planilha) da classe Herdada
+				// XSSFWorkbook(POI Apache) a partir do arquivo fs lido
+				// - Pode gerar exceção IOException se o objeto não for uma
+				// planilha xlsx
 
-	
+			XSSFSheet sheet = workbook.getSheet("Plan1"); // Criado objeto de planilha a partir da classe XSSFSheet, a
+															// partir do Workbook
+
+			int numRows = sheet.getLastRowNum(); // Método da classe XSSFSheet que pega número de linhas da planilha
+			int numCols = sheet.getRow(1).getLastCellNum(); // Método da classe XSSFSheet que pega número de colunas
+															// naquela linha
+
+			for (int r = 0; r <= numRows; r++) { // For para percorrer as linhas da planilha
+
+				XSSFRow row = sheet.getRow(r); // Cria um objeto de linha com o método getRow na posição do for
+
+				for (int c = 0; c < numCols; c++) { // Para cada linha, será percorrido o numero de colunas obtido
+													// anteriormente
+
+					XSSFCell cell = row.getCell(c); // Em cada coluna, será obtida a célula
+
+					switch (cell.getCellType()) { // Verifica qual o tipo de conteúdo da célula para ler de acordo
+
+					case STRING:
+						System.out.print(cell.getStringCellValue());
+						break; // Se for string, chamada o método para ler e sai do switch
+					case NUMERIC:
+						System.out.print(cell.getNumericCellValue());
+						break;
+					case BOOLEAN:
+						System.out.print(cell.getBooleanCellValue());
+						break;
+					case BLANK:
+						System.out.print("Empty cell!");
+						break;
+					default:
+						break;
+					
+					}
+					System.out.print(" | "); // Separador para formatação sendo executado dentro do for
+				}
+				System.out.println(); // Separador para formatação sendo executado dentro do for das linhas
+			}
+			inputstream.close();
+			workbook.close();
+		}
+
+		catch (FileNotFoundException e) {
+			System.out.println("Arquivo não encontrado!");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
--- src/exceloperations/ReadingExcel.java
+++ src/exceloperations/ReadingExcel.java
@@ -2,85 +2,23 @@
 
 package exceloperations;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
-
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import entities.ExcelFile;
 
-public class ReadingExcel { 
+public class ReadingExcel {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Digite o caminho do arquivo: ");
 		String path = sc.nextLine();
-		
+
 		sc.close();
-		
-	ExcelFile excelfile = new ExcelFile(path);
-	
-	
-		
-		String excelFilePath = (excelfile.getPath());
-		try {
-			FileInputStream inputstream = new FileInputStream(excelFilePath); // Inicializa leitor de arquivos genérico - fs
-			XSSFWorkbook workbook=new XSSFWorkbook(inputstream);; // Inicializa objeto workbook (Planilha) da classe Herdada
-															// XSSFWorkbook(POI Apache) a partir do arquivo fs lido
-															// - Pode gerar exceção IOException se o objeto não for uma
-															// planilha xlsx
 
-			XSSFSheet sheet = workbook.getSheet("Plan1"); // Criado objeto de planilha a partir da classe XSSFSheet, a
-															// partir do Workbook
-
-			int numRows = sheet.getLastRowNum(); // Método da classe XSSFSheet que pega número de linhas da planilha
-			int numCols = sheet.getRow(1).getLastCellNum(); // Método da classe XSSFSheet que pega número de colunas
-															// naquela linha
-
-			for (int r = 0; r <= numRows; r++) { // For para percorrer as linhas da planilha
-
-				XSSFRow row = sheet.getRow(r); // Cria um objeto de linha com o método getRow na posição do for
-
-				for (int c = 0; c < numCols; c++) { // Para cada linha, será percorrido o numero de colunas obtido
-													// anteriormente
-
-					XSSFCell cell = row.getCell(c); // Em cada coluna, será obtida a célula
-
-					switch (cell.getCellType()) { // Verifica qual o tipo de conteúdo da célula para ler de acordo
-
-					case STRING:
-						System.out.print(cell.getStringCellValue());
-						break; // Se for string, chamada o método para ler e sai do switch
-					case NUMERIC:
-						System.out.print(cell.getNumericCellValue());
-						break;
-					case BOOLEAN:
-						System.out.print(cell.getBooleanCellValue());
-						break;
-					}
-					System.out.print(" | "); // Separador para formatação sendo executado dentro do for
-				}
-				System.out.println(); // Separador para formatação sendo executado dentro do for das linhas
-			}
-			inputstream.close();
-			
-		}
-
-		catch (FileNotFoundException e) {
-			System.out.println("Arquivo não encontrado!");
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		ExcelFile excelfile = new ExcelFile(path);
+		excelfile.ReadFromFile();
 
 	}
 }
-
-
