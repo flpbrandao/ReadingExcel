3f0da94fc99f1101587094166b1ed55e1e5a62b6
Felipe Oliveira <flpbrandao@hotmail.com>
2021-06-13 12:00:41 -0300

    Projeto iniciado, sem interação de path

---
+++ src/application/program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Scanner;
+
+import exceloperations.ReadingExcel;
+
+public class program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digitar caminho do arquivo: ");
+		String path = sc.nextLine();
+
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
